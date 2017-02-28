133134d59abb119e26a3ab7f2a3b5b022298e94d
Leidy Rosmery <rosm3rita@hotmail.com>
2017-02-28 15:56:45 -0500

    Commit Version 3

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+function calculaIMC(){
+  var peso, altura, imc, leyenda;
+  peso=prompt("Ingrese el Dato de Peso");
+  altura=prompt("Ingrese el Dato de la Altura");
+  imc=peso/(altura*altura);
+  alert( " IMC :"  + imc.toFixed(4) );
+}
