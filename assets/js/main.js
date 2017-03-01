662b55e8d7771000b72c7c0168ecc800d5679f05
Maria <mariacutipa.gonzales@gmail.com>
2017-03-01 11:38:52 -0500

    fusion5

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,6 @@ function calcularIMC() {
     altura=document.getElementById("talla").value/100;
     imc=peso/(altura*altura);
 
-    alert( " IMC :"  + imc.toFixed(4) );
+    alert( " IMC :"  + imc.toFixed(2));
 
     }
